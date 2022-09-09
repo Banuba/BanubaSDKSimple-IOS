feb5bbec79202d6f87b6ffc933450d23561174f6
sdk-banuba <71497187+sdk-banuba@users.noreply.github.com>
2022-09-09 19:00:02 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
 // swift-tools-version:5.3
 import PackageDescription
 let package = Package(
-  name: "BanubaSdkSimple",
+  name: "BanubaSDKSimple",
   platforms: [
     .iOS(.v11)
   ],
   products: [
     .library(
-      name: "BanubaSdkSimple",
-      targets: ["BanubaSdkSimple"])
+      name: "BanubaSDKSimple",
+      targets: ["BanubaSDKSimple"])
   ],
   targets: [
     .binaryTarget(
-      name: "BanubaSdkSimple",
-      path: "BanubaSdkSimple.xcframework")
+      name: "BanubaSDKSimple",
+      path: "BanubaSDKSimple.xcframework")
   ])
