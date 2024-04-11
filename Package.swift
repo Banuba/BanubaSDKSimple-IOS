aeebce4597afa8e70bd7a8dc80369a7757cbf8aa
user <user@users-Mac-mini.local>
2024-04-11 13:52:16 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -1,17 +1,22 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
+
 import PackageDescription
+
 let package = Package(
   name: "BanubaSdkSimple",
   platforms: [
-    .iOS(.v12)
+    .iOS(.v15)
   ],
   products: [
     .library(
       name: "BanubaSdkSimple",
-      targets: ["BanubaSdkSimple"])
+      targets: ["BanubaSdkSimple"]
+    )
   ],
   targets: [
     .binaryTarget(
       name: "BanubaSdkSimple",
-      path: "BanubaSdkSimple.xcframework")
-  ])
+      path: "BanubaSdkSimple.xcframework"
+    )
+  ]
+)
