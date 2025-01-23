1df8109b36f4b8fa720370ce4f92a1893862478f
CI <ci@banuba.com>
2025-01-23 14:26:13 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,16 +2,22 @@
 
 import PackageDescription
 
+let videoEditorSDKRange: Range<Version> = "1.40.0"..<"1.49.99"
+
 let package = Package(
   name: "BanubaSdkSimple",
   platforms: [
-    .iOS(.v17)
+    .iOS(.v15)
   ],
   products: [
     .library(
       name: "BanubaSdkSimple",
       targets: ["BanubaSdkSimple"]
     )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", videoEditorSDKRange),
+    .package(url: "https://github.com/Banuba/BanubaVideoEditorCore-iOS.git", videoEditorSDKRange)
   ],
   targets: [
     .binaryTarget(
